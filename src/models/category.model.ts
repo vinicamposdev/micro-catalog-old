ecf7424503de4fa048fdcc405e1d3e749b03e555
Vinicius Campos <viniciusmoliveirac@hotmail.com>
2021-08-17 23:53:51 -0300

    feat: add category model

---
+++ src/models/category.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Category extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Category>) {
+    super(data);
+  }
+}
+
+export interface CategoryRelations {
+  // describe navigational properties here
+}
+
+export type CategoryWithRelations = Category & CategoryRelations;
